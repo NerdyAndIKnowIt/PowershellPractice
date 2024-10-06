be1ef0aabeed284ba07e903130538838f06c7053
Lab Admin <LabAdmin@consultingfirm.com>
2024-10-05 22:24:49 -0700

    fixed New-ADUser error, added error handling

--- Requirements2/Restore-AD.ps1
+++ Requirements2/Restore-AD.ps1
@@ -1,50 +1,57 @@
 <#
 made by Breanna Chase, student ID 011725162
 #>
+try {
+    #check for the existance of the finance OU
+    $OUName = "ou=Finance,dc=consultingfirm,dc=com"
+    $OUFinanceExists = Get-ADOrganizationalUnit -Filter {Name -eq "Finance"} -ErrorAction SilentlyContinue 
+    #"ErrorAction SilentlyContinue" will bypass any error if the finance OU does not exist
 
-#check for the existance of the finance OU
-$OUName = "ou=Finance,dc=consultingfirm,dc=com"
-$OUFinanceExists = Get-ADOrganizationalUnit -Filter {Name -eq "Finance"} -ErrorAction SilentlyContinue 
-#"ErrorAction SilentlyContinue" will bypass any error if the finance OU does not exist
+    if ($OUFinanceExists) {
+        Write-Host "OU finance exists"
+        Remove-ADOrganizationalUnit -Identity $OUName -Recursive -Confirm:$false
+        Write-Host "OU finance has been deleted"
+    }
+    else {
+        Write-Host "OU finance does not exist"
+    }
 
-if ($OUFinanceExists) {
-    Write-Host "OU finance exists"
-    # Set-ADOrganizationalUnit -Identity $OUName -ProtectedFromAccidentialDeletion $false
-    Remove-ADOrganizationalUnit -Identity $OUName -Recursive -Confirm:$false
-    Write-Host "OU finance has been deleted"
+    #create the finance OU
+    New-ADOrganizationalUnit -Name "Finance" -Path "DC=consultingfirm,DC=com" -ProtectedFromAccidentalDeletion $False
+    Write-Host "OU finance created"
+
+    #import users from financePersonnel.csv file into the finance OU
+    $csvPath = Join-Path -Path (Get-Location) -ChildPath "\financePersonnel.csv"
+    $csvData = Import-Csv -Path $csvPath
+
+    foreach ($user in $csvData) {
+        #create the display name using the first and last name
+        $DisplayName = "$($user.First_Name) $($user.Last_Name)"
+
+        #as each row is iterated through in financePersonnel.csv, create a new user with New-ADUser
+        New-ADUser  -GivenName = $user.First_Name `
+            -Surname $user.Last_Name `
+            -DisplayName $DisplayName `
+            -SamAccountName $user.samAccount `
+            -PostalCode $user.PostalCode `
+            -OfficePhone $user.OfficePhone `
+            -MobilePhone $user.MobilePhone `
+            -Path $OUName `
+            -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) `
+            -Enabled $true
+        
+    }
+
+    #generate an output file for submission
+    Get-ADUser -Filter * -SearchBase $OUName -Properties DisplayName,PostalCode,OfficePhone,MobilePhone | 
+        Select-Object DisplayName, PostalCode, OfficePhone, MobilePhone |
+        Out-File -FilePath ".\AdResults.txt"
+
+    Write-Host "adResults.txt has been updated"
 }
-else {
-    Write-Host "OU finance does not exist"
+#if there is an error at any time during execution, display it to the console
+catch {
+    Write-Host "There was an error: "
+    Write-Host $_.Exception.Message
+    Exit
 }
-
-#create the finance OU
-New-ADOrganizationalUnit -Name "Finance" -Path "DC=consultingfirm,DC=com" -ProtectedFromAccidentalDeletion $False
-Write-Host "OU finance created"
-
-#import users from financePersonnel.csv file into the finance OU
-$csvPath = Join-Path -Path (Get-Location) -ChildPath "\financePersonnel.csv"
-$csvData = Import-Csv -Path $csvPath
-
-Write-Host $csvPath
-Write-Host $csvData
-foreach ($row in $csvData) {
-    $DisplayName = "$($row.First_Name) $($row.Last_Name)"
-
-    New-ADUser -GivenName $row.First_Name `
-        -Surname $row.Last_Name `
-        -DisplayName $DisplayName `
-        -SamAccountName $samAccount `
-        -PostalCode $row.PostalCode `
-        -OfficePhone $row.OfficePhone `
-        -MobilePhone $row.MobilePhone `
-        -Path $OUName `
-        -AccountPassword (ConvertTo-SecureString "password" -AsPlainText -Force) `
-        -Enabled $true
-}
-
-#generate an output file for submission
-Get-ADUser -Filter * -SearchBase $OUName -Properties DisplayName,PostalCode,OfficePhone,MobilePhone | 
-    Select-Object DisplayName, PostalCode, OfficePhone, MobilePhone |
-    Out-File -FilePath ".\AdResults.txt"
-
-Write-Host "adResults.txt has been updated"
