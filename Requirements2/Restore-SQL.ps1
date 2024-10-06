528bab23437eb2d5f3459953c8d4bce03c914767
Lab Admin <LabAdmin@consultingfirm.com>
2024-10-06 15:39:27 -0700

    final version of Restore-AD and Restore-SQL, fixed various syntax errors

--- Requirements2/Restore-SQL.ps1
+++ Requirements2/Restore-SQL.ps1
@@ -2,54 +2,60 @@
 made by Breanna Chase, student ID 011725162
 #>
 
+$databaseName = "ClientDB"
+$serverInstance = "localhost\SQLEXPRESS"
+
 #path to the CSV file
 $csvPath =  ".\NewClientData.csv"
 
 #check if client DB exists
 try {
-    $dbExists = Invoke-Sqlcmd - Query "select name from sys.database where name 'ClientDB'" -ServerInstance "localhost\SQLEXPRESS"
+    $dbExists = Get-SqlDatabase -ServerInstance $serverInstance -Database $databaseName
 
-    if ($dbExists) {
-        Write-Host "ClientDB database already exists, deleting it now"
-        Invoke-Sqlcmd -Query "drop database ClientDB" -ServerInstance "localhost\SQLEXPRESS"
-        Write-Host "ClientDB database has been deleted"
+    if ($null -eq $dbExists) {
+
+        Write-Host "ClientDB database does not exist"
     }
     else {
-        Write-Host "ClientDB database does not exist"
+        Write-Host "ClientDB database already exists, deleting it"
+        Invoke-Sqlcmd -Query "alter database ClientDB set single_user with rollback immediate; drop database ClientDB;" -ServerInstance $serverInstance
+        Write-Host "ClientDB database has been deleted"
     }
 
     #create a new database called ClientDB
-    Invoke-Sqlcmd -Query "create database ClientDB" -ServerInstance "localhost\SQLEXPRESS"
+    Invoke-Sqlcmd -Query "create database ClientDB" -ServerInstance $serverInstance
     Write-Host "Database ClientDB has been created"
 
     #create a table called Client_A_Contacts
-    Invoke-Sqlcmd -Database ClientDB -Query @"
-        create table dbo.Client_A_Contacts (
-            FirstName nvarchar(25)
-            LastName nvarchar(25)
-            City nvarchar(25)
-            Country nvarchar(25)
-            Zip nvarchar(5)
-            officePhone nvarchar(10)
-            mobilePhone nvarchar(10)
+    Invoke-Sqlcmd -Database $databaseName -ServerInstance $serverInstance -Query @"
+        create table Client_A_Contacts (
+            first_name nvarchar(25),
+            last_name nvarchar(25),
+            city nvarchar(25),
+            county nvarchar(25),
+            zip varchar(5),
+            officePhone nvarchar(12),
+            mobilePhone nvarchar(12)
         )
-"@ -ServerInstance "localhost\SQLEXPRESS"
+"@
     Write-Host "the Client_A_Contacts table has been created"
 
     #insert data from csv into the table
     $csvData = Import-Csv -Path $csvPath
     foreach ($row in $csvData) {
         Invoke-Sqlcmd -Database ClientDB @"
-            insert into dbo.Client_A_Contacts (FirstName, LastName, City, Country, Zip, officePhone, mobilePhone)
-            values ('$($row.FirstName)','$($row.LastName)','$($row.City)','$($row.Country)','$($row.Zip)','$($row.officePhone)','$($row.mobilePhone)')
-"@ -ServerInstance "localhost\SQLEXPRESS"
+            insert into Client_A_Contacts (first_name, last_name, city, county, zip, officePhone, mobilePhone)
+            values ('$($row.first_name)','$($row.last_name)','$($row.city)','$($row.county)','$($row.zip)','$($row.officePhone)','$($row.mobilePhone)')
+"@ -ServerInstance $serverInstance
     }
     Write-Host "client data was imported into Client_A_Contacts from the NewClientData.csv file"
 
     #generate an output file
-    Invoke-Sqlcmd -Database ClientDB -ServerInstance "localhost\SQLEXPRESS" -Query "select * from dbo.Client_A_Contacts" > .\SqlResults.txt
+    Invoke-Sqlcmd -Database ClientDB -ServerInstance $serverInstance -Query "select * from dbo.Client_A_Contacts" > .\SqlResults.txt
     Write-Host "results have been written to SqlResults.txt"
 
-} catch {
+} 
+#catch any errors and write them to the console
+catch {
     Write-Host "there was an error: $_"
 }
